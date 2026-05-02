6b1f3d289fdea87e642bca95cc0c4b49cfb9472f
hisyamyasidp <hisyamyasidp@gmail.com>
2026-05-02 15:57:29 +0700

    ADD Gripper Control

--- components/Gripper/Gripper.cpp
+++ components/Gripper/Gripper.cpp
@@ -1,17 +1,23 @@
 #include "Gripper.hpp"
 
-Gripper::Gripper(Servo* servo) : servo(servo) {}
+Gripper::Gripper(Servo* servo) : servo(servo), max_angle(90), min_angle(0) {}
 
-void Gripper::increase_angle() {
+void Gripper::close() {
   this->current_angle += this->angle_step;
   this->servo->set_angle(this->current_angle);
 }
 
-void Gripper::decrease_angle() {
+void Gripper::open() {
   this->current_angle -= this->angle_step;
   this->servo->set_angle(this->current_angle);
 }
 
-float Gripper::get_angle() {
-  return this->current_angle;
+void Gripper::lift() {
+  this->current_angle = this->max_angle;
+  this->servo->set_angle(this->current_angle);
+}
+
+void Gripper::drop() {
+  this->current_angle = this->min_angle;
+  this->servo->set_angle(this->current_angle);
 }
--- components/Gripper/Gripper.hpp
+++ components/Gripper/Gripper.hpp
@@ -5,11 +5,15 @@ class Gripper {
 private:
   Servo* servo;
   float current_angle = 0;
-  float angle_step = 5.0; // Increment/decrement per keypress
+  float angle_step = 5.0;
+  float max_angle;  // <-- tambah ini
+  float min_angle;  // <-- tambah ini
 
 public:
   Gripper(Servo* servo);
   void increase_angle();
   void decrease_angle();
+  void lift();        // <-- tambah ini
+  void drop();        // <-- tambah ini
   float get_angle();
 };
