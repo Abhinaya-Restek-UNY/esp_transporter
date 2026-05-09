4b6d8077f076fe787a0747def827015d0ba19989
Febri Bayu Nurcahyo <febribayun@gmail.com>
2026-05-09 22:10:16 +0700

    test.

--- main/transporter.cpp
+++ main/transporter.cpp
@@ -2,6 +2,7 @@
 #include "IMU.hpp"
 #include "Mecanum.hpp"
 #include "MotorHub.hpp"
+#include "esp_task_wdt.h"
 #include "freertos/idf_additions.h"
 #include "freertos/projdefs.h"
 #include "nvs_flash.h"
@@ -20,23 +21,30 @@ extern "C" void app_main(void) {
   Gamepad gamepad;
   gamepad.start();
 
-  IMU imu(GPIO_NUM_21, GPIO_NUM_22, GPIO_NUM_35);
-  imu.start(6);
+  // IMU imu(GPIO_NUM_21, GPIO_NUM_22, GPIO_NUM_35);
+  // imu.start(6);
 
   MotorHub mt_hub(0, 20'000);
   mt_hub.start();
 
-  Motor *motor1 = mt_hub.create_motor(GPIO_NUM_19, GPIO_NUM_5, GPIO_NUM_18);
-  Motor *motor2 = mt_hub.create_motor(GPIO_NUM_25, GPIO_NUM_27, GPIO_NUM_26);
-  Motor *motor3 = mt_hub.create_motor(GPIO_NUM_4, GPIO_NUM_2, GPIO_NUM_32);
-  Motor *motor4 = mt_hub.create_motor(GPIO_NUM_15, GPIO_NUM_23, GPIO_NUM_14);
+  // A1
+  Motor *mt_fr = mt_hub.create_motor(GPIO_NUM_19, GPIO_NUM_18, GPIO_NUM_23);
+  // B1
+  Motor *mt_fl = mt_hub.create_motor(GPIO_NUM_33, GPIO_NUM_5, GPIO_NUM_32);
+  // A2
+  Motor *mt_br = mt_hub.create_motor(GPIO_NUM_27, GPIO_NUM_14, GPIO_NUM_13);
+  // B2
+  Motor *mt_bl = mt_hub.create_motor(GPIO_NUM_26, GPIO_NUM_25, GPIO_NUM_17);
 
-  Mecanum::PIDConf pid_conf{.Kp = 1.5, .Ki = 0.5, .Kd = 0.1, .delta_t = 10};
+  // Mecanum::PIDConf pid_conf{.Kp = 1.5, .Ki = 0.5, .Kd = 0.1, .delta_t = 10};
 
-  Mecanum mecanum(motor1, motor2, motor3, motor4, &imu, &pid_conf);
+  Mecanum mecanum(mt_fr, mt_fl, mt_br, mt_bl
+                  // ,&imu, &pid_conf
+  );
 
   Gamepad::joy_data_t orientation;
   Gamepad::joy_data_t direction;
+
   while (1) {
 
     if (gamepad.is_pressed(Gamepad::ButtonCode::PS_BUTTON)) {
@@ -47,9 +55,9 @@ extern "C" void app_main(void) {
     gamepad.get_l_joy(&direction);
     gamepad.get_r_joy(&orientation);
 
-    mecanum.set_angle(atan2(orientation.y, orientation.x));
+    // mecanum.set_angle(atan2(orientation.y, orientation.x));
 
-    mecanum.set_direction(direction.x << 6, direction.y << 6);
+    mecanum.set_direction(direction.x, -direction.y);
 
     mecanum.update();
 
