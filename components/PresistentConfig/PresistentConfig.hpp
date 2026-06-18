2c7831014a1478bd421bc1abe52fa984bdd2d086
Febri Bayu Nurcahyo <febribayun@gmail.com>
2026-06-18 08:36:19 +0700

    fix(presistentconfig): Fixing some compiler error when using presistent config to store array.

--- components/PresistentConfig/PresistentConfig.hpp
+++ components/PresistentConfig/PresistentConfig.hpp
@@ -1,30 +1,41 @@
 #pragma once
 
 #include "nvs_flash.h"
+#include <cstring>
 #include <string>
+#include <type_traits>
 
 template <typename T> class PresistentConfig {
 public:
-  // Constructor defined inline
-  PresistentConfig(const std::string &key, T default_value)
+  PresistentConfig(const std::string &key, const T &default_value)
       : key(key), handle(0) {
     size_t required_size = sizeof(T);
-    this->value = default_value;
+
+    if constexpr (std::is_array_v<T>) {
+      std::memcpy(&this->value, &default_value, sizeof(T));
+    } else {
+      this->value = default_value;
+    }
+
     auto ret = nvs_open("storage", NVS_READWRITE, &this->handle);
     if (ret == ESP_OK) {
       if (nvs_get_blob(handle, this->key.c_str(), &this->value,
                        &required_size) != ESP_OK ||
           required_size != sizeof(T)) {
-        this->value = default_value;
+
+        if constexpr (std::is_array_v<T>) {
+          std::memcpy(&this->value, &default_value, sizeof(T));
+        } else {
+          this->value = default_value;
+        }
       }
     } else {
-      printf("Failed to open NVS: %s", esp_err_to_name(ret));
+      printf("Failed to open NVS: %s\n", esp_err_to_name(ret));
     }
   }
 
   T value;
 
-  // Methods defined inline
   void save() {
     if (handle) {
       nvs_set_blob(handle, this->key.c_str(), &this->value, sizeof(T));
@@ -40,7 +51,6 @@ public:
     }
   }
 
-  // Destructor defined inline
   ~PresistentConfig() {
     if (handle) {
       nvs_close(handle);
