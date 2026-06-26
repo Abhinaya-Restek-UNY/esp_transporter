57afa48c83568f4754cf29e3cce53132051d8e29
Febri Bayu Nurcahyo <febribayun@gmail.com>
2026-06-26 23:03:10 +0700

    add: configurable claw close angle and save them to nvs

--- components/Gripper/Gripper.cpp
+++ components/Gripper/Gripper.cpp
@@ -3,34 +3,28 @@
 #include <cmath>
 #include <cstdio>
 
-#define MAX_CLAW_ANGLE 95
-#define MIN_CLAW_ANGLE 70
+#define MAX_CLAW_ANGLE 90
+#define MIN_CLAW_ANGLE 55
 
-#define MAX_LIFT_ANGLE 85
+#define MAX_LIFT_ANGLE 78
 #define MIN_LIFT_ANGLE 0
 
-Gripper::Gripper(Servo *claw, Servo *lifter, uint32_t debounce)
-    : claw(claw), lifter(lifter) {
+Gripper::Gripper(Servo *claw, Servo *lifter, float _max_claw,
+                 const std::string &config_key)
+    : claw(claw), lifter(lifter), max_angle(config_key, _max_claw) {
   this->set_claw(true);
   this->set_lifter(0);
 }
 
 void Gripper::set_claw(bool gripping) {
-  this->claw->set_angle(gripping ? MIN_CLAW_ANGLE : MAX_CLAW_ANGLE);
+  this->claw->set_angle(gripping ? MIN_CLAW_ANGLE : this->max_angle.value);
 }
 void Gripper::set_lifter(int16_t throttlebrake) {
-  static double half = (MAX_LIFT_ANGLE - MIN_LIFT_ANGLE) * 0.6;
+  static double half = (MAX_LIFT_ANGLE - MIN_LIFT_ANGLE) * .5;
   if (throttlebrake > 10) {
-
-    this->lifter->set_angle(half * pow(1.0 - throttlebrake / 1024.0, 3));
+    int16_t val = throttlebrake < 256 ? 256 : throttlebrake;
+    this->lifter->set_angle(half * (1.0 - pow(val / 1024.0, 3)));
   } else {
     this->lifter->set_angle(MAX_LIFT_ANGLE);
   }
 }
-
-uint32_t Gripper::get_time() { return esp_timer_get_time() / 1000; }
-
-Gripper::Gripper() {
-  // NOTE: YES DOES NOTHING.
-
-};
--- components/Gripper/Gripper.hpp
+++ components/Gripper/Gripper.hpp
@@ -1,26 +1,19 @@
+
 #pragma once
-#include "Servo.hpp"
+#include "PresistentConfig.hpp"
+#include "ServoManager.hpp"
 #include "esp_timer.h"
 
 class Gripper {
 private:
   Servo *claw;
   Servo *lifter;
-  bool claw_is_open = false;
-  bool lifter_is_open = false;
-  float current_angle = 0;
-  float claw_step = 0.1;
-  float lifted_angle = 0;
-  float drop_angle = 0;
-
-  uint32_t last_claw_toggle = this->get_time();
-  uint32_t last_lifter_toggle = this->get_time();
-  uint32_t debounce = 500;
-  uint32_t get_time();
 
 public:
-  Gripper();
-  Gripper(Servo *claw, Servo *lifter, uint32_t debounce);
+  Gripper(Servo *claw, Servo *lifter, float max_claw,
+          const std::string &config_key);
+
+  PresistentConfig<float> max_angle;
   void set_claw(bool gripping);
   void set_lifter(int16_t throttlebreak);
 };
